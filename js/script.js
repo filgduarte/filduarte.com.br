e453cd73d97e42a10c3012a39303e831c740e958
Filipe Duarte <filipegduarte@gmail.com>
2023-07-29 17:55:38 -0300

    add: content and animations for work and layout sections

--- js/script.js
+++ js/script.js
@@ -11,11 +11,32 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 window.addEventListener('load', function () {
+    const body = document.querySelector('body');
+    const background = document.querySelector('.background');
     const ripples = document.querySelector('.ripples');
     const heroImage = document.querySelector('.hero-image');
     const profilePicWidth = heroImage.querySelector('img').offsetWidth;
     const ripplesInitialLeft = Math.round(heroImage.offsetHeight / 5) * 2 + 32;
+    const heroContainer = document.querySelector('.hero-content');
+    const myWork = document.querySelector('#my-work');
     const translateAmount = heroImage.offsetWidth - profilePicWidth;
+    const socialItems = document.querySelectorAll('.contact-social li');
+
+    socialItems.forEach( contactItem => {
+        const fakeItem = document.querySelector(`.fake .${contactItem.className}`);
+        const fakeItemOffset = {
+            x: fakeItem.offsetLeft,
+            y: fakeItem.offsetTop,
+        };
+        const contactItemOffset = {
+            x: contactItem.offsetLeft,
+            y: contactItem.offsetTop,
+        };
+        const translateX = contactItemOffset.x - fakeItemOffset.x;
+        const translateY = contactItemOffset.y - fakeItemOffset.y;
+
+        fakeItem.style.setProperty("--translate-amount", `${translateX}px, ${translateY}px`);
+    });
 
     ripples.addEventListener('transitionstart', function() {
         const rippleItems = ripples.querySelectorAll('.ripple');
@@ -36,8 +57,6 @@ window.addEventListener('load', function () {
     ripples.style.setProperty("--translate-amount", `calc(${translateAmount}px - 50%)`);
 
     window.addEventListener('scroll', function() {
-        const background = document.querySelector('.background');
-        const heroContainer = document.querySelector('.hero-content');
         const initialviewportReference = heroContainer.offsetTop + heroImage.offsetTop;
         const rect = heroImage.getBoundingClientRect();
         background.style.top = rect.top - initialviewportReference + 'px';
@@ -46,13 +65,48 @@ window.addEventListener('load', function () {
         {
             ripples.style.willChange = 'transform';
             heroImage.style.willChange = 'background-position, mask-position';
-            document.querySelector('body').classList.add('past-hero');
+            body.classList.add('past-hero');
         }
         else
         {
-            document.querySelector('body').classList.remove('past-hero');
+            body.classList.remove('past-hero');
+        }
+
+        if ( scrollTrigger('#my-work', 25, 'bottom') )
+        {
+            if ( ! myWork.classList.contains('on-viewport') )
+            {
+                // Check if has class .on-viewport
+                // so it doesn't reset the toggler
+                const works = document.querySelectorAll('.work-item');
+                works.forEach(workItem => {
+                    workItem.style.setProperty('--toggler', 1);
+                });
+
+                myWork.classList.add('on-viewport');
+            }
+        }
+        else
+        {
+            myWork.classList.remove('on-viewport');
+        }
+
+        if ( scrollTrigger('#contact .layout-container', 'bottom', 'bottom') )
+        {
+            body.classList.add('contact-visible');
+        }
+        else
+        {
+            body.classList.remove('contact-visible');
         }
     });
+
+    document.querySelector('.work-item:last-child').addEventListener('transitionend', function() {
+        const works = document.querySelectorAll('.work-item');
+        works.forEach(workItem => {
+            workItem.style.setProperty('--toggler', 0);
+        });
+    })
 });
 
 const headerLinks = document.querySelectorAll('#main-header a');
@@ -67,9 +121,14 @@ headerLinks.forEach( headerLink => {
         }
         else
         {
-            lenis.scrollTo( e.currentTarget.getAttribute("href") );
+            lenis.scrollTo( target );
         }
 	});
+});
+
+document.querySelector('aside .button-primary').addEventListener('click', function(e) {
+    e.preventDefault();
+    lenis.scrollTo( e.currentTarget.getAttribute("href") );
 });
 
 const menuToggleButton = document.querySelector('.menu-toggle');
@@ -125,3 +184,58 @@ function scrollTrigger(trigger, triggerReference, viewportReference) {
 
     return triggerTop <= (viewportReferencePosition - triggerReferencePosition);
 }
+
+// #TODO: Use IntersectionObserver instead of EventListener
+// function scrollTrigger(selector, options = {}) {
+//     const keywords = {
+//         'top': 0,
+//         'center': 0.5,
+//         'bottom': 1,
+//     }
+
+//     options.threshold = options.threshold[keywords] ?? options.threshold;
+
+//     let elements = document.querySelectorAll(selector);
+//     elements.forEach(el => {
+//         addObserver(el, options);
+//     });
+// }
+
+// function addObserver(element, options) {
+//     if ( ! ('IntersectionObserver' in window) )
+//     {
+//         if (options.callback)
+//         {
+//             options.callback(element);
+//         }
+//         else
+//         {
+//             entry.target.classList.add('active');
+//         }
+
+//         return
+//     }
+
+//     let observer = new IntersectionObserver((entries, observer) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting)
+//             {
+//                 if (options.callback)
+//                 {
+//                     options.callback(element);
+//                 }
+//                 else
+//                 {
+//                     entry.target.classList.add('visible')
+//                 }
+
+//                 if (options.once)
+//                 {
+//                     observer.unobserve(entry.target);
+//                 }
+//             }
+//         });
+//     }, options);
+
+//     observer.observe(element);
+// }
