101eb223eb615b94aaa8d4bca74f42368b80e798
Filipe Duarte <filipegduarte@gmail.com>
2023-08-08 10:18:30 -0300

    change: sendmail.php path correction

--- src/scripts/sendmail.js
+++ src/scripts/sendmail.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
     const formData = new FormData(form);
 
-    fetch("/src/scripts/sendmail.php", {
+    fetch("./sendmail.php", {
         method: "POST",
         body: formData
     })
