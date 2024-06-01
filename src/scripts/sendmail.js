b44ecdd0244b6d24f71d28b9a12eb47098ed316a
Filipe Duarte <filipegduarte@gmail.com>
2024-06-01 18:37:32 -0300

    change: added english  translation to sendmail.js messages

--- src/scripts/sendmail.js
+++ src/scripts/sendmail.js
@@ -4,6 +4,7 @@ const resultContainer = document.querySelector('.submit-message');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     const formData = new FormData(form);
+    const isEnglish = window.location.href.includes('/en');
 
     fetch("./sendmail.php", {
         method: "POST",
@@ -13,28 +14,38 @@ form.addEventListener('submit', function(event) {
     .then(data => {
         if (data.success)
         {
-            resultContainer.innerText = "Obrigado por entrar em contato! Sua mensagem foi enviada.";
+            resultContainer.innerText = isEnglish
+                                        ? "Thank you for getting in touch! Your message has been sent."
+                                        : "Obrigado por entrar em contato! Sua mensagem foi enviada.";
             return true;
         }
 
         if (data.empty)
         {
             const emptyFields = data.empty.join(', ');
-            resultContainer.innerText += `Por favor, preencha os campos: ${emptyFields}\n`;
+            resultContainer.innerText += isEnglish
+                                        ? `Please, fill in the fields: ${emptyFields}\n`
+                                        : `Por favor, preencha os campos: ${emptyFields}\n`;
         }
 
         if (data.invalidEmail)
         {
-            resultContainer.innerText += "Por favor, use um email válido.";
+            resultContainer.innerText += isEnglish
+                                        ? "Please use a valid email address."
+                                        : "Por favor, use um email válido.";
         }
 
         if (data.fail)
         {
-            resultContainer.innerText = "Houve um erro enviando a mensagem, tente novamente.";
+            resultContainer.innerText = isEnglish
+                                        ? "There was an error sending the message, please try again."
+                                        : "Houve um erro enviando a mensagem, tente novamente.";
         }
     })
     .catch(error => {
         console.error("Fetch error: ", error);
-        resultContainer.innerText = "Houve um erro enviando a mensagem, tente novamente.";
+        resultContainer.innerText = isEnglish
+                                    ? "There was an error with the 'send message' request. =("
+                                    : "Houve um erro ao solicitar o envio da mensagem. =(";
     });
 });
